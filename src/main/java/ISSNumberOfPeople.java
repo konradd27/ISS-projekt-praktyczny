88b95043d9d0de95ff88e7a0f716d18c9b6d53f4
konradd27 <konradd27@gmail.com>
2023-02-01 20:02:46 +0100

    ISSNumberOfPeople working

--- src/main/java/ISSNumberOfPeople.java
+++ src/main/java/ISSNumberOfPeople.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
 public class ISSNumberOfPeople {
+    public Integer getNumberOfPeople ()throws Exception{
+        ISSNumberOfPeople_Deserialize issNumberOfPeople_deserialize = new ISSNumberOfPeople_Deserialize();
+        ISSNumberOfPeopleDto issNumberOfPeopleDto = issNumberOfPeople_deserialize.getIssNumberOfPeopleDto();
+        return issNumberOfPeopleDto.getNumber();
+    }
+
+    public List<ISSPeople> getListOfPeople() throws Exception{
+        ISSNumberOfPeople_Deserialize issNumberOfPeople_deserialize = new ISSNumberOfPeople_Deserialize();
+        ISSNumberOfPeopleDto issNumberOfPeopleDto = issNumberOfPeople_deserialize.getIssNumberOfPeopleDto();
+        return issNumberOfPeopleDto.getPeople();
+    }
 }
