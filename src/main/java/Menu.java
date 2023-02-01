f8dc9a0e49256afc3563bb4a1e9dfc492708144a
STJimmmy <tox85@mail.ru>
2023-02-01 19:59:29 +0100

    Changed menu (no more recursions)

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,50 +1,101 @@
 import java.util.Scanner;
 
 public class Menu {
+    private boolean isRunning = true;
+    private static final String OPTIONS = """
+            What d u wana do?
+            1 - obliczania prędkości ISS        
+            2 - zwracanie listy nadchodzących przebiegów ISS dla określonej lokalizacji
+            3 - zwracanie liczby osób przebywających w kosmosie w ramach ISS
+            4 - GUI - pozycja ISS na mapie swiata
+            5 - dodatkowa statystika
+            6 - zakoncz program""";
+private static final String MENUERRORMESSAGE = "Please enter a number 1-6";
 
-    static void showUserMenu(){
-        System.out.println("What d u wana do?");
-        System.out.println("""
-        1 - obliczania prędkości ISS        
-        2 - zwracanie listy nadchodzących przebiegów ISS dla określonej lokalizacji
-        3 - zwracanie liczby osób przebywających w kosmosie w ramach ISS
-        4 - GUI - pozycja ISS na mapie swiata
-        5 - dodatkowa statystika
-        6 - zakoncz program""");
-        while(true){
-            Scanner scanner = new Scanner(System.in);
-            try{Integer userInput = Integer.parseInt(scanner.next());
-                userSelectionProcess(userInput);
-                break;
-            }catch (Exception e){
-                System.out.println("Please choose a valid option, don`t write letters");
-            }
+    public void startMenu() {
+        do {
+            menuAction();
+        } while (isRunning);
+    }
+
+    public void showMenuOptions() {
+        System.out.println(OPTIONS);
+    }
+
+    private void menuAction() {
+        showMenuOptions();
+        try {
+            int input = readInt();
+            handleOption(input);
+        } catch (Exception e) {
+            System.out.println(MENUERRORMESSAGE);
         }
-    }
-static void userSelectionProcess(int userInput){
 
-    switch (userInput){
-        case 1:
-            System.out.println(userInput);
-            break;
-        case 2:
-            ISSPass.issPass();
-            break;
-        case 3:
-            System.out.println(userInput);
-            break;
-        case 4:
-            System.out.println(userInput);
-            break;
-        case 5:
-            System.out.println(userInput);
-            break;
-        case 6:
-            System.out.println("program terminated");
-            break;
-        default:
-            System.out.println("!!!Please choose a valid option 1-6!!!");
-            showUserMenu();
     }
+
+    private void handleOption(int input) {
+
+        executeOption(input);
+
+    }
+
+    private void executeOption(int input) {
+
+        switch (input) {
+            case 1:
+                try{
+                    ISSSpeed issSpeed = new ISSSpeed();
+                    double speed = issSpeed.getSpeed();
+                    System.out.println("ISS current speed is " + speed + " M/S");
+                    break;
+                } catch (Exception e){e.printStackTrace();}
+
+            case 2:
+                ISSPass.issPass();
+                break;
+            case 3:
+                System.out.println(input);
+                break;
+            case 4:
+                System.out.println(input);
+                break;
+            case 5:
+                System.out.println(input);
+                break;
+            case 6:
+                System.out.println("Program Terminated");
+                isRunning = false;
+                break;
+            default:
+                System.out.println(MENUERRORMESSAGE);
+
+        }
+
+    }
+
+
+    private void end() throws Exception {
+        showMessage("Program Terminated");
+        isRunning = false;
+
+    }
+
+    void showMessage(String message) {
+    }
+
+    ;
+
+    int readInt() {
+
+        Scanner scanner = new Scanner(System.in);
+        Integer input = Integer.parseInt(scanner.next());
+        return input;
+
+
+    }
+
 }
-}
+
+
+
+
