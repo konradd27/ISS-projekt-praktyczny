ccf92fdf21c0afadb4169e3489a905710baed705
GrzegorzMar <grzegorz.maruszewski@gmail.com>
2023-02-01 17:58:44 +0100

    aded API class

---
+++ src/main/java/ISSNumberOfPeople_API.java
@@ -0,0 +1,29 @@
+import okhttp3.*;
+
+public class ISSNumberOfPeople_API {
+    private final static String BASE_URL = "http://api.open-notify.org/astros.json";
+
+    public static void main(String[] args) throws Exception {
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+
+        Response response = call.execute();
+
+        System.out.println(response.body().string());
+    }
+
+    public static String getJSON() throws Exception{
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+
+        Response response = call.execute();
+
+        return response.body().string();
+    }
+}
